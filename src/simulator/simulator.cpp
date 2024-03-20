ebfc730fdd21d15fff15fd3aef75d6a09b15be9f
G Côte <meltwin158@gmail.com>
2024-03-20 15:57:29 +0100

    TODO: Simulator implement flon, flat

--- src/simulator/simulator.cpp
+++ src/simulator/simulator.cpp
@@ -13,7 +13,7 @@ Simulator::Simulator(NodeOptions opts) : Node(NODE_NAME, opts) {
       Wheel(declare_parameter("left_wheel_diameter_m", DEF_WHEEL_D),
             declare_parameter("left_gear_ratio", DEF_GEAR_RATIO),
             declare_parameter("left_tick_per_rev", DEF_TICK_PER_REV)),
-      MAX_TICK_CHANGE, declare_parameter("base_width_m", DEF_BASE));
+      MAX_TICK_CHANGE, declare_parameter("base_width_m", DEF_BASE), 1.0, 1.0);
 
   // Declare plugins
   plugins.push_back(plugins::Motor::make(robot));
