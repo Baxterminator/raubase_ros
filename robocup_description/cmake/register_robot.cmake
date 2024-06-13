415153ec12ebade317ffc9560d8429621c6971cd
G Côte <meltwin158@gmail.com>
2024-06-13 18:47:09 +0200

    Installing resources files (URDF, mesh, ...)

--- robocup_description/cmake/register_robot.cmake
+++ robocup_description/cmake/register_robot.cmake
@@ -1,18 +1,24 @@
 include("cmake/to_snake_case.cmake")
 include("cmake/generate_robot_files.cmake")
 
-macro(register_robot rname)
+macro(make_robot_package rname)
+  cmake_parse_arguments(ARG "" "URDF" "RESOURCES_DIR" ${ARGN})
   to_snake_case(${rname} pkg_name)
-  message("Generating robot: ${rname} (${pkg_name})")
+  message("Generating robot package: ${rname} (${pkg_name})")
 
   # Get source generation folder
   generate_folders(${pkg_name} source_folder build_directory)
-  message("\t-> Generated sources folder: ${source_folder}")
+  message("   -> Generated sources folder: ${source_folder}")
 
-  # Generate files
+  # Generate building files
   generate_cmakelist(${pkg_name} ${source_folder})
   generate_package(${pkg_name} ${source_folder})
 
-  add_subdirectory(${source_folder} ${build_directory})
+  # Copy resources files
+  if (ARG_RESOURCES_DIR)
+    install_resources(${pkg_name} ${ARG_RESOURCES_DIR})
+  endif()
 
+  # Launch to the CMake stack
+  add_subdirectory(${source_folder} ${build_directory})
 endmacro()
